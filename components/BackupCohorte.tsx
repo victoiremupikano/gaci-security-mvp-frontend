e3b6707b2cbf94fbdbfedd0bf1091ccb9fef9d11
victoiremupikano <ushindimupikano@gmail.com>
2023-06-01 16:09:25 +0200

    comp backup cohorte

--- components/BackupCohorte.tsx
+++ components/BackupCohorte.tsx
@@ -5,7 +5,7 @@ import moment from "moment";
 import "moment/locale/fr"
 
 const BackupCohorte: FunctionComponent = () => {
-    const [Cohorte, setCohorte] = useState<any[]>()
+    const [cohorte, setCohorte] = useState<any[]>()
     const getCohorte = async() => {
       const result = await Cohorte.get()
       if (result.results) {
@@ -14,40 +14,50 @@ const BackupCohorte: FunctionComponent = () => {
   }
   useEffect(() => {
     getCohorte()
-    localStorage.setItem("current_report", 'all-Cohorte')
-    localStorage.setItem("filename", 'all_Cohorte_backup')
+    localStorage.setItem("current_report", 'all-cohorte')
+    localStorage.setItem("filename", 'all_cohorte_backup')
   },[])
     return (
       <div id="all-Cohortes" className="w-11/12 h-full mx-auto">
         <div className="flex my-4 justify-center">
           <h1 className="w-80 font-semibold">
-            LISTE DE TOUS LES EXERCICES
+            LISTE DE TOUS LES COHORTES
           </h1>
         </div>
         <div className="w-full p-2 border-b mb-2 font-semibold text-gray-700 md:text-base text-xs hidden md:flex justify-between">
           <span className="w-[5%]">#</span>
-          <span className="w-[15%]">Idsl</span>
-          <span className="w-[25%]">Début</span>
-          <span className="w-[20%]">Limite</span>
-          <span className="w-[25%] flex">Status</span>
+          <span className="w-[10%]">Idsl</span>
+          <span className="w-[20%]">Libélé</span>
+          <span className="w-[10%]">Début</span>
+          <span className="w-[10%]">Limite</span>
+          <span className="w-[5%]">Nbr Hosp.</span>
+          <span className="w-[5%]">Nbr Amb.</span>
+          <span className="w-[5%]">% Hosp.</span>
+          <span className="w-[5%]">% Amb.</span>
+          <span className="w-[5%] flex">Status</span>
           <span className="w-[10%] flex justify-center">Créé</span>
           <span className="w-[10%]">Modif.</span>
         </div>
         <div className="w-full flex text-xs flex-col  md:justify-between">
-          {Array.isArray(Cohorte) && Cohorte.length > 0
-            ? Cohorte.map((u) => (
+          {Array.isArray(cohorte) && cohorte.length > 0
+            ? cohorte.map((u) => (
                 <div key={u.pk}>
                   <div
                     className={`w-full ${
-                      Cohorte.indexOf(u) % 2 != 0 ? "bg-gray-200" : ""
+                      cohorte.indexOf(u) % 2 != 0 ? "bg-gray-200" : ""
                     } p-2 mb-2  text-gray-700  md:flex hidden justify-between`}
                   >
-                    <span className="w-[5%]">{Cohorte.indexOf(u) + 1}</span>
-                    <span className="w-[15%]">{u?.idsl}</span>
-                    <span className="w-[25%]">{u?.dateStart}</span>
-                    <span className="w-[20%]">{u?.dateEnd}</span>
+                    <span className="w-[5%]">{cohorte.indexOf(u) + 1}</span>
+                    <span className="w-[10%]">{u?.idsl}</span>
+                    <span className="w-[20%]">{u?.wording}</span>
+                    <span className="w-[10%]">{u?.dateStart}</span>
+                    <span className="w-[10%]">{u?.dateEnd}</span>
+                    <span className="w-[5%]">{u?.nbrHosp}</span>
+                    <span className="w-[5%]">{u?.nbrAmbu}</span>
+                    <span className="w-[5%]">{u?.pourcentHosp}</span>
+                    <span className="w-[5%]">{u?.pourcentAmbu}</span>
                     <span
-                    className={`w-[7.5%] font-semibold ${
+                    className={`w-[5%] font-semibold ${
                       u.status ? "text-green-600" : ""
                       }`}
                       >
