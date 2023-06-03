9af09f29b5dad7171df2d91e0a2248b55b21cfac
victoiremupikano <ushindimupikano@gmail.com>
2023-06-03 10:40:55 +0200

    sms module

---
+++ api/sms.ts
@@ -0,0 +1,29 @@
+import { ResponseGlobal } from "../types";
+import api from "./index";
+
+declare type FnAPIResponse = Record<string,any> & {
+  type?: "success" | "error" | "auth";
+  data?: any;
+  status?: number;
+  statusText?: string;
+  token?: any;
+  pk?: any;
+  results?: any[];
+} & ResponseGlobal
+
+const SMS = {
+  async add(data: Record<string, any>, entreprize_id:string): Promise<FnAPIResponse> {
+    return api.post("/sms-message-list-create/" + entreprize_id + "/", data, 'share_param');
+  },
+  async delete(id:string): Promise<FnAPIResponse> {
+    return api.delete("/sms-message-delete/"+id+'/', 'share_param');
+  },
+  async getAll(entreprize_id:string): Promise<FnAPIResponse> {
+    return api.get("/sms-message-list-create/" + entreprize_id, 'share_param');
+  },
+  async get(id:string | number, entreprize_id:string): Promise<FnAPIResponse> {
+    return api.get("/sms-message-detail/" + id + "/" + entreprize_id, 'share_param');
+  },
+};
+
+export default SMS;
