30cc6cfcfb7e728a923cb52be6f500b60b0f60c7
victoiremupikano <ushindimupikano@gmail.com>
2023-06-16 16:16:07 +0200

    mise de la partie unconected dans staff

--- pages/staff/unconected/pub/sections/section3.tsx
+++ pages/staff/unconected/pub/sections/section3.tsx
@@ -1,11 +1,13 @@
 import "moment/locale/fr";
 import { useEffect, useState } from "react";
-import Post from "../../../../api/post";
-import PostSection3Model from "../../../../components/PostSection3ModelUC";
+import Post from "../../../../../api/post";
+import PostSection3Model from "../../../../../components/PostSection3ModelUC";
 
 export default function section3() {
   const [postsRepost, setPostsRepost] = useState<Array<any>>();
   const [loadingRepost, setLoadingRepost] = useState(false);
+  const [postsFavorite, setPostsFavorite] = useState<Array<any>>();
+  const [loadingFavorite, setLoadingFavorite] = useState(false);
 
   const getPostsRepost = async (entreprizeId: string) => {
     setLoadingRepost(true);
@@ -15,9 +17,18 @@ export default function section3() {
     }
     setLoadingRepost(false);
   };
+  const getPostsFavorite = async (entreprizeId: string) => {
+    setLoadingFavorite(true);
+    const result = await Post.getPostAllPublishedAndFavorisByUserLogged(entreprizeId);
+    if (result.results) {
+      setPostsFavorite(result.results);
+    }
+    setLoadingFavorite(false);
+  };
   useEffect(() => {
     const entreprize = localStorage.getItem("entreprize");
     getPostsRepost(entreprize as string);
+    getPostsFavorite(entreprize as string);
   }, []);
 
   return (
@@ -30,6 +41,25 @@ export default function section3() {
             {!loadingRepost ? (
               Array.isArray(postsRepost) && postsRepost.length > 0 ? (
                 postsRepost.map((a, index) => (
+                  <PostSection3Model key={index} post={a} />
+                ))
+              ) : (
+                "No data"
+              )
+            ) : (
+              <div className="flex h-40 w-full font-semibold  items-center justify-center">
+                Loading...
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="item">
+          <h1 className="font-bold text-4xl py-12">Favoris</h1>
+          <div className="flex flex-col gap-6">
+            {/* posts */}
+            {!loadingFavorite ? (
+              Array.isArray(postsFavorite) && postsFavorite.length > 0 ? (
+                postsFavorite.map((a, index) => (
                   <PostSection3Model key={index} post={a} />
                 ))
               ) : (
